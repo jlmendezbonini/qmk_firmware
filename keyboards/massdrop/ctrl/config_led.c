82f8e5c4b6eaa661094abe22e52ef85c6e284bd8
Jorge Mendez <jlmendezbonini@gmail.com>
2021-05-03 22:36:23 -0700

    Update keymap

--- keyboards/massdrop/ctrl/config_led.c
+++ keyboards/massdrop/ctrl/config_led.c
@@ -72,11 +72,11 @@ led_config_t g_led_config = { {
 } };
 
 
-#ifdef USB_LED_INDICATOR_ENABLE
-void rgb_matrix_indicators_kb(void)
-{
-  md_rgb_matrix_indicators();
-}
-#endif // USB_LED_INDICATOR_ENABLE
+// #ifdef USB_LED_INDICATOR_ENABLE
+// void rgb_matrix_indicators_kb(void)
+// {
+//   md_rgb_matrix_indicators();
+// }
+// #endif // USB_LED_INDICATOR_ENABLE
 
 #endif // RGB_MATRIX_ENABLE

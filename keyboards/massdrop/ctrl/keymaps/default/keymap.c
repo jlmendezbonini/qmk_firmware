228decfa354d831cadde2b587f22889dc1804f98
Jorge Mendez <jlmendezbonini@gmail.com>
2021-06-22 22:04:56 -0700

    Create function layer.

--- keyboards/massdrop/ctrl/keymaps/default/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/default/keymap.c
@@ -11,50 +11,67 @@ enum ctrl_keycodes {
 };
 
 enum layers {
-    QWERTY,    
+    QWERTY = 0,    
     XCODE,
     VSCODE,
     CONFIG,
+    FUNCTION,
 };
+
+#define DEFINE_QWERTY    0
+#define DEFINE_XCODE     1
+#define DEFINE_VSCODE    2
+#define DEFINE_CONFIG    3
+#define DEFINE_FUNCTION  4
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [QWERTY] = LAYOUT(
-        KC_ESC,  KC_F14,  KC_F15,  RGB_VAD,     RGB_VAI,  LCTL(KC_UP), KC_F11,   LALT(LCMD(KC_ESC)),   DM_REC1, DM_PLY1, KC_MPRV, KC_MPLY, KC_MNXT,            KC_VOLD,              KC_VOLU,                           TO(1), \
+        KC_ESC,  KC_F14,  KC_F15,  RGB_VAD,     RGB_VAI,  LCTL(KC_UP), KC_F11,   LALT(LCMD(KC_ESC)),   DM_REC1, DM_PLY1, KC_MPRV, KC_MPLY, KC_MNXT,            KC_VOLD,              KC_VOLU,                           TO(DEFINE_XCODE), \
         KC_GRV,  KC_1,    KC_2,    KC_3,        KC_4,     KC_5,                 KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,   KC_EQL,  KC_BSPC,     LCMD(LSFT(KC_LBRC)),  LCMD(LSFT(KC_RBRC)),                KC_PGUP, \
         KC_TAB,  KC_Q,    KC_W,    KC_E,        KC_R,     KC_T,                 KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,   KC_RBRC, KC_BSLS,     KC_DEL,               LCTL(LALT(LCMD(KC_SPC))),           KC_PGDOWN,\
         KC_CAPS, KC_A,    KC_S,    KC_D,        KC_F,     KC_G,                 KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,   KC_ENT, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,        KC_V,     KC_B,                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,                           KC_UP, \
-        KC_LCTL, KC_LALT, KC_LCMD,                           KC_SPC,           KC_RCMD, KC_RALT, LCTL(LALT(LCMD(KC_SPC))),    KC_RCTL,                 KC_LEFT, KC_DOWN, KC_RGHT \
+        KC_LCTL, KC_LALT, KC_LCMD,                           KC_SPC,           KC_RCMD, KC_RALT, MO(DEFINE_FUNCTION),    KC_RCTL,                                 KC_LEFT, KC_DOWN, KC_RGHT \
     ),
     [XCODE] = LAYOUT(
-        KC_ESC,  KC_F14,  KC_F15,  RGB_VAD,     RGB_VAI,  LCTL(KC_UP), KC_F11,   LALT(LCMD(KC_ESC)),   DM_REC1, DM_PLY1, KC_MPRV, KC_MPLY, KC_MNXT,  KC_VOLD,   KC_VOLU,            TO(2), \
+        KC_ESC,  KC_F14,  KC_F15,  RGB_VAD,     RGB_VAI,  LCTL(KC_UP), KC_F11,   LALT(LCMD(KC_ESC)),   DM_REC1, DM_PLY1, KC_MPRV, KC_MPLY, KC_MNXT,  KC_VOLD,   KC_VOLU,            TO(DEFINE_VSCODE), \
         KC_GRV,  KC_1,    KC_2,    KC_3,        KC_4,     KC_5,                 KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,   KC_EQL,  KC_BSPC,    LCTL(LCMD(KC_J)),     KC_F6,                LCTL(LALT(LCMD(KC_SPC))), \
         KC_TAB,  KC_Q,    KC_W,    KC_E,        KC_R,     KC_T,                 KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,   KC_RBRC, KC_BSLS,    KC_DEL,               LCMD(LSFT(KC_LBRC)),  LCMD(LSFT(KC_RBRC)), \
         KC_CAPS, KC_A,    KC_S,    KC_D,        KC_F,     KC_G,                 KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,   KC_ENT, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,        KC_V,     KC_B,                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,                           KC_UP, \
-        KC_LCTL, KC_LALT, KC_LCMD,                           KC_SPC,           KC_RCMD, KC_RALT, LCTL(LALT(LCMD(KC_SPC))),    KC_RCTL,                 KC_LEFT, KC_DOWN, KC_RGHT \
+        KC_LCTL, KC_LALT, KC_LCMD,                           KC_SPC,           KC_RCMD, KC_RALT,  _______,    KC_RCTL,                 KC_LEFT, KC_DOWN, KC_RGHT \
     ),
     [VSCODE] = LAYOUT(
-        KC_ESC,  KC_F14,  KC_F15,  RGB_VAD,     RGB_VAI,  LCTL(KC_UP), KC_F11,   LALT(LCMD(KC_ESC)),   DM_REC1, DM_PLY1, KC_MPRV, KC_MPLY, KC_MNXT,  KC_VOLD,   KC_VOLU,            TO(3), \
+        KC_ESC,  KC_F14,  KC_F15,  RGB_VAD,     RGB_VAI,  LCTL(KC_UP), KC_F11,   LALT(LCMD(KC_ESC)),   DM_REC1, DM_PLY1, KC_MPRV, KC_MPLY, KC_MNXT,  KC_VOLD,   KC_VOLU,            TO(DEFINE_CONFIG), \
         KC_GRV,  KC_1,    KC_2,    KC_3,        KC_4,     KC_5,                 KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,   KC_EQL,  KC_BSPC,      KC_F12,               KC_F6,                LCTL(LALT(LCMD(KC_SPC))), \
         KC_TAB,  KC_Q,    KC_W,    KC_E,        KC_R,     KC_T,                 KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,   KC_RBRC, KC_BSLS,      KC_DEL,               LCMD(LSFT(KC_LBRC)),  LCMD(LSFT(KC_RBRC)), \
         KC_CAPS, KC_A,    KC_S,    KC_D,        KC_F,     KC_G,                 KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,   KC_ENT, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,        KC_V,     KC_B,                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,                           KC_UP, \
-        KC_LCTL, KC_LALT, KC_LCMD,                           KC_SPC,           KC_RCMD, KC_RALT, LCTL(LALT(LCMD(KC_SPC))),    KC_RCTL,                 KC_LEFT, KC_DOWN, KC_RGHT \
+        KC_LCTL, KC_LALT, KC_LCMD,                           KC_SPC,           KC_RCMD, KC_RALT,  _______,    KC_RCTL,                 KC_LEFT, KC_DOWN, KC_RGHT \
     ),
     [CONFIG] = LAYOUT(
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                          KC_NO, KC_NO, TO(0), \
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                          KC_NO, KC_NO, TO(DEFINE_QWERTY), \
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                   KC_NO, KC_NO, KC_NO, \
         KC_NO, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, KC_NO, U_T_AUTO, U_T_AGCR, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO, \
         KC_NO, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
         KC_NO, RGB_TOG, KC_NO, KC_NO, KC_NO, MD_BOOT, NK_TOGG, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                    KC_MS_U, \
         KC_NO, KC_NO, KC_NO,                   KC_NO,                            KC_NO, KC_NO, KC_NO, KC_NO,                  KC_MS_L, KC_MS_D, KC_NO \
     ),
+    [FUNCTION] = LAYOUT(
+        KC_ESC,  KC_F14,  KC_F15,  RGB_VAD,     RGB_VAI,  LCTL(KC_UP), KC_F11,   LALT(LCMD(KC_ESC)),   DM_REC1, DM_PLY1, KC_MPRV, KC_MPLY, KC_MNXT,  KC_VOLD,   KC_VOLU,            TO(DEFINE_CONFIG), \
+        KC_GRV,  KC_1,    KC_2,    KC_3,        KC_4,     KC_5,                 KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,   KC_EQL,  KC_DEL,       KC_F12,               KC_F6,                LCTL(LALT(LCMD(KC_SPC))), \
+        KC_TAB,  KC_Q,    KC_W,    KC_E,        KC_R,     KC_T,                 KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,   KC_RBRC, KC_BSLS,      KC_DEL,               LCMD(LSFT(KC_LBRC)),  LCMD(LSFT(KC_RBRC)), \
+        KC_CAPS, KC_A,    KC_S,    KC_D,        KC_F,     KC_G,                 KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,   KC_ENT, \
+        KC_LSFT, KC_Z,    KC_X,    KC_C,        KC_V,     KC_B,                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,                           KC_UP, \
+        KC_LCTL, KC_LALT, KC_LCMD,                           KC_SPC,           KC_RCMD, KC_RALT,  _______,    KC_RCTL,                 KC_LEFT, KC_DOWN, KC_RGHT \
+    ),
+
 };
 
 //       ESC
 //     ___0___, _______, _______, _______, ___4___, _______, _______, _______, _______, ___9__, _______, _______,  ___12__,           __13___, __14___, __15___,
-//     ___16__, _______, __18___, _______, _______, _______, _______, __23___, _______, ______, _______, __27___,  _______, _______,  ___30__, _______, __32___,
+//     ___16__, _______, __18___, _______, _______, _______, _______, __23___, _______, ______, _______, __27___,  _______, __29___,  ___30__, _______, __32___,
 //     __ 33__, __34___, __35___, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  ___47___, _______, _______,
 //     ___50__, __51___,___52___, _______, _______, _______, _______, _______, _______, _______, _______, _______, ___62__,
 //     ___63__, __64___, __65___, __66___, __67___, __68___, _______, ___70__, _______, _______, _______, ___74__,                              ___75___,
@@ -100,6 +117,11 @@ void rgb_matrix_indicators_kb(void) {
                 rgb_matrix_set_color(64, RGB_RED);
                 rgb_matrix_set_color(68, RGB_RED);
                 break;
+
+            case FUNCTION:
+                rgb_matrix_set_color_all(RGB_BLACK);
+                rgb_matrix_set_color(29, RGB_YELLOW);
+                break;
         }
     }
 
@@ -124,6 +146,7 @@ void keyboard_post_init_user(void) {
 #define MODS_SHIFT  (get_mods() & MOD_MASK_SHIFT)
 #define MODS_CTRL   (get_mods() & MOD_MASK_CTRL)
 #define MODS_ALT    (get_mods() & MOD_MASK_ALT)
+
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     static uint32_t key_timer;
@@ -194,6 +217,24 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
               }
             }
             return false;
+            
+        // case M_BSDEL: {
+        //     uint8_t kc = KC_BSPC;
+            
+        //     if (keyboard_report->mods & MOD_BIT(KC_LSFT)) {
+        //         kc = KC_DEL;
+        //         bsdel_mods = true;
+        //     } else {
+        //         bsdel_mods = false;
+        //     }
+
+        //     if (record->event.pressed) {
+        //         register_code (kc);
+        //     } else {
+        //         unregister_code (kc);
+        //     }
+        // }
+
         default:
             return true; //Process all other keycodes normally
     }
